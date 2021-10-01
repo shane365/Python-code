7cfb1274662db0e946c40e14d8c95afe460163d4
Shane <69674845+shane365@users.noreply.github.com>
2021-10-01 22:20:47 +0530

    Add files via upload

---
+++ test code.py
@@ -0,0 +1,16 @@
+import nump
+
+def isZigzag(numbers):
+    #create array & initiate to 0
+    arr = list(len(numbers) - 2)
+    i = 0
+    while (i < len(arr) - 2):
+        if (numbers[i] > numbers[i+1]) and (numbers[i+1] < numbers[i+2]) or (numbers[i] < numbers[i+1]) and (numbers[i+1] > numbers[i+2]):
+            arr[i] = 1
+        i = i + 1
+    return arr
+
+
+***
+https://www.linkedin.com/in/lester-s-fernandes-a641b51/
+https://www.facebook.com/lester.fernandes.146
